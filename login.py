02764447b52cad8bdf75c2ad068422679717175a
BaSalehi <baran_salehi@mathdep.iust.ac.ir>
2024-12-29 19:59:55 +0330

    sign_up features

--- login.py
+++ login.py
@@ -1,5 +1,9 @@
 import os
 import keyboard
+import json
+import uuid
+
+users_json = 'users.json'
 
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
@@ -31,4 +35,77 @@ def main_menu():
             break
 
     return options[selected_index]
+
+
+def sign_up():
+    user = input("Username: ")
+    pas = input("Password: ")
+    email = input("Email: ")
+    user_id = str(uuid.uuid4())
+    users = load_users()
+
+
+    if user == "exit" or pas == "exit" or email == "exit":
+        main_menu()
+    
+    email_check = check_email(email)
+
+    while not email_check:
+        print("please enter a correct email")
+        email = input("Email: ")
+        email_check = check_email(email)
+
+    user_info ={'ID': user_id,
+                'username': user,
+                'email': email,
+                'password': pas}
+    
+    user_check = check_user(users, user, email)
+
+    if not user_check:
+        return "Username or email is already taken. Please try again."
+    else:
+        add_user(user_info)
+
+def check_email(email):
+    import re
+
+    pattern = re.compile(r'[a-zA-Z0-9._+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}')
+
+    emails = pattern.findall(email)
+
+    if emails:
+        return True
+    else:
+        return False
+
+def load_users():
+    if os.path.exists('users.json'):
+        with open('users.json', 'r') as file:
+            return json.load(file)
+    return []
+
+def add_user(user):
+    try:
+        with open('users.json', 'r') as file:
+            users = json.load(file)
+    except FileNotFoundError:
+        users = []
+
+    users.append(user)
+
+    with open('users.json', 'w') as file:
+        json.dump(users, file, indent=4)
+
+def check_user(users, username, email):
+    if users == []:
+        return True
+    for user in users:
+        if user['username'] == username or user['email'] == email:
+            return False
+    return True
+
+choice = main_menu()
+if choice == "Sign Up":
+    sign_up()
 
