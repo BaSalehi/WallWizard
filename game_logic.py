36314b38f83d31bf501e13d8cc880d861d47366e
soroushxy <90596063+soroushxy@users.noreply.github.com>
2025-01-12 21:18:08 +0330

    logic is complited

--- game_logic.py
+++ game_logic.py
@@ -1,37 +1,108 @@
 import copy
 import random
+import os
+import json 
+# import login
+
+from rich.console import Console
+from rich.table import Table
+from rich.text import Text
+# import user_iterface
+
+class Game:
+    def __init__(self, id, user1, user2, user1_position, user2_position, user1_walls, user2_walls, game_board, current_player):
+        self.id = id
+        self.user1 = user1
+        self.user2 = user2
+        self.user1_position = user1_position
+        self.user2_position = user2_position
+        self.user1_walls = user1_walls
+        self.user2_walls = user2_walls
+        self.game_board = game_board
+        self.current_player = current_player
+        self.winner = 0
+
+    
+
+def update_item(file_path, item_id, new_value):
+
+    with open(file_path, 'r') as file:
+        games = json.load(file)
+    new_game = list()    
+    for item in games:
+        if item['ID'] == item_id:
+            new_game.append(new_value)
+        else:
+            new_game.append(item) 
+    file2 = open(file_path, 'w') 
+    i =json.dumps(new_game , indent=4)
+    file2.write(i)
+    file2.close()
+
+def generate_id():
+    with open('games.json', 'r') as file:
+        games = json.load(file)
+    new_id = len(games) + 1
+    return new_id
+
+game_id = generate_id()
+
+def add_game(game_info):
+    games = []
+    if os.path.exists('games.json'):
+        try:
+            with open('games.json', 'r') as file:
+                games = json.load(file)
+        except json.JSONDecodeError:
+            print("Error reading JSON file. Initializing with an empty list.")
+            games = []
+        except Exception as e:
+            print(f"An error occurred: {e}")
+            return
+    games.append(game_info)
+    try:
+        with open('games.json', 'w') as file:
+            json.dump(games, file, indent=4)
+    except Exception as e:
+        print(f"An error occurred while writing to the file: {e}")
+
 
 def matrix():
     return [[0 for i in range(17)] for j in range(17)]
 
-matrix = matrix()
-matrix[0][8] = 1
-matrix[16][8] = 2
-player1_pos = (0, 8)
-player2_pos = (16, 8)
+
 
 def show_board(matrix):
+    console = Console()
+    table = Table(show_header=False, box=None)  
+
+    headers = [" "]
+    headers.extend([str(i//2 + 1) if i % 2 == 0 else " " for i in range(17)])
+    table.add_row(*headers)
+    
     for i in range(17):
-        if i % 2 == 0:
-            for j in range(17):
+        row = [str(i//2 + 1) if i % 2 == 0 else " "]  
+        for j in range(17):
+            if i % 2 == 0:
                 if j % 2 == 0 and matrix[i][j] == 0:
-                    print('+', end='')
+                    cell = Text("+", style="bold green")  
+                elif j % 2 == 0 and matrix[i][j] != 0:
+                    cell = Text(str(matrix[i][j]), style="bold blue")
+                elif j % 2 != 0 and matrix[i][j] == 0:
+                    cell = " "
+                elif j % 2 != 0 and matrix[i][j] != 0:
+                    cell = Text(str(matrix[i][j]), style="bold blue")
+            else:
                 if j % 2 == 0 and matrix[i][j] != 0:
-                    print(matrix[i][j], end='')
-                if j % 2 != 0 and matrix[i][j] == 0:
-                    print(' ', end='')
-                if j % 2 != 0 and matrix[i][j] != 0:
-                    print(matrix[i][j], end='')
-            print()
-        else:
-            for j in range(17):
-                if j%2 == 0 and matrix[i][j] != 0 :
-                    print(matrix[i][j], end='')
-                if j%2 == 0 and matrix[i][j] == 0:
-                    print(' ', end='')
-                if j%2 != 0 :
-                    print(' ', end='')
-            print()
+                    cell = Text(str(matrix[i][j]), style="bold blue")
+                elif j % 2 == 0 and matrix[i][j] == 0:
+                    cell = " "
+                elif j % 2 != 0:
+                    cell = " "
+            row.append(cell)
+        table.add_row(*row)
+
+    console.print(table)
 
 
 def is_valid_move_player(matrix, player_pos, move):
@@ -62,27 +133,46 @@ def is_special_move(matrix, player_pos, move):
     if move == 'down-right':
         if (x + 3 < len(matrix) and y + 3 < len(matrix[0]) and
             ((matrix[x+2][y] != 0 and matrix[x+1][y] == 0 and matrix[x+3][y] != 0) or 
-             (matrix[x][y+1] == 0 and matrix[x][y+2] != 0 and matrix[x][y+3] != 0)) and 
+             (matrix[x][y+1] == 0 and matrix[x][y+2] != 0 and matrix[x][y+3] != 0)) and
             is_valid_move_player(matrix, player_pos, 'down-to-right')):
             return True
+        elif(((x+2 == 16 and matrix[x+1][y]==0 and matrix[x+2][y]!=0)or
+             (y==2 and matrix[x][1]==0 and matrix[x][0] != 0) and
+             is_valid_move_player(matrix, player_pos, 'down-to-right'))):
+            return True
+
     if move == 'down-left':
         if (x + 3 < len(matrix) and y - 3 >= 0 and
             ((matrix[x+2][y] != 0 and matrix[x+1][y] == 0 and matrix[x+3][y] != 0) or 
-             (matrix[x][y-1] == 0 and matrix[x][y-2] != 0 and matrix[x][y-3] != 0)) and 
+             (matrix[x][y-1] == 0 and matrix[x][y-2] != 0 and matrix[x][y-3] != 0)) and
             is_valid_move_player(matrix, player_pos, 'down-to-left')):
+            return True
+        elif (((x+2 == 16 and matrix[x+1][y]==0 and matrix[x+2][y]!=0) or
+             (y==2 and matrix[x][1]==0 and matrix[x][0] != 0)) and 
+              is_valid_move_player(matrix, player_pos, 'down-to-left')):
             return True
     if move == 'up-right':
         if (x - 3 >= 0 and y + 3 < len(matrix[0]) and
             ((matrix[x-2][y] != 0 and matrix[x-1][y] == 0 and matrix[x-3][y] != 0) or 
-             (matrix[x][y+1] == 0 and matrix[x][y+2] != 0 and matrix[x][y+3] != 0)) and 
+             (matrix[x][y+1] == 0 and matrix[x][y+2] != 0 and matrix[x][y+3] != 0))and
             is_valid_move_player(matrix, player_pos, 'up-to-right')):
             return True
+        elif(((x-2 == 0 and matrix[x-1][y]==0 and matrix[x-2][y] !=0)or
+             (y==14 and matrix[x][15]==0 and matrix[x][16]!=0)) and
+             is_valid_move_player(matrix, player_pos, 'up-to-right')):
+            return True
+
     if move == 'up-left':
         if (x - 3 >= 0 and y - 3 >= 0 and
             ((matrix[x-2][y] != 0 and matrix[x-1][y] == 0 and matrix[x-3][y] != 0) or 
-             (matrix[x][y-1] == 0 and matrix[x][y-2] != 0 and matrix[x][y-3] != 0)) and 
+             (matrix[x][y-1] == 0 and matrix[x][y-2] != 0 and matrix[x][y-3] != 0)) and
             is_valid_move_player(matrix, player_pos, 'up-to-left')):
             return True
+        elif(((x-2 == 0 and matrix[x-1][y]==0 and matrix[x-2][y]!=0)or
+              (y==14 and matrix[x][15]==0 and matrix[x][16]!=0)) and
+              is_valid_move_player(matrix, player_pos, 'up-to-left')):
+            return True
+
     if move == 'double_up':
         if x > 2 and matrix[x-1][y]==0 and matrix[x-2][y]!=0 and matrix[x-3][y]==0:
             return True
@@ -96,7 +186,6 @@ def is_special_move(matrix, player_pos, move):
         if y < 14 and matrix[x][y+1]==0 and matrix[x][y+2]!=0 and matrix[x][y+3]==0:
             return True 
     return False
-
 def special_moves(matrix, player_pos):
     possible_moves = []
     special_moves = ['down-right', 'down-left', 'up-right', 'up-left','double_up','double_down','double_left','double_right']
@@ -287,70 +376,130 @@ def is_winner2(player_pos):
         return True
     return False
 
-
-player1_walls = 10
-player2_walls = 10
-current_player = random.choice([1, 2])
-counter = 0
-
-while True:
-    if counter >11:
+def main_game(matrix , player1_pos , player2_pos , player1_walls , player2_walls , current_player , game_id , user1,user2):
+    while True:
         if is_winner1(player1_pos):
             print("Player 1 won!")
             break
         elif is_winner2(player2_pos):
             print("Player 2 won!")
             break
-  
-
-    print(f"It is the turn of player {current_player}")
-    print(f"Player 1 walls: {player1_walls} | Player 2 walls: {player2_walls}")
-    action = input("Do you want to 'move' or 'place wall'? ").strip().lower()
-    valid_moves = all_valid_moves(matrix, player1_pos if current_player == 1 else player2_pos)
-    
-    if action == 'move':
-        move = input(f"Enter move {valid_moves}: ").strip()
-        if current_player == 1:
-            if move in valid_moves:
-                player1_pos = move_player(matrix, player1_pos, move, 1)
-                counter += 1
+        print('game id:',game_id)
+        print(f'player1:{user1} | player2:{user2}')
+        print(f"It is the turn of {user1 if current_player == 1 else user2}")
+        print(f"Player 1 walls: {player1_walls} | Player 2 walls: {player2_walls}")
+        action = input("Do you want to 'move' or 'place wall'? ").strip().lower()
+        valid_moves = all_valid_moves(matrix, player1_pos if current_player == 1 else player2_pos)
+        if action == 'move':
+            move = input(f"Enter move {valid_moves}: ").strip()
+            if current_player == 1:
+                if move in valid_moves:
+                    player1_pos = move_player(matrix, player1_pos, move, 1)
+                elif move == 'exit':
+                    break
+                else:
+                    print("Invalid move! Try again.")
+                    continue
             else:
-                print("Invalid move! Try again.")
-                continue
-        else:
-            if move in valid_moves:
-                player2_pos = move_player(matrix, player2_pos, move, 2)
-                counter += 1
-            else:
-                print("Invalid move! Try again.")
-                continue
-
-    elif action == 'place wall':
-        if (current_player == 1 and player1_walls > 0) or (current_player == 2 and player2_walls > 0):
-            i = int(input("Enter wall position (row): "))
-            j = int(input("Enter wall position (column): "))
-            orientation = input("Enter wall orientation (e.g., 'up-right', 'down-left'): ").strip().split('-')
-            if is_valid_wall(matrix, (i, j), orientation[0], orientation[1]):
-                new_matrix = copy.deepcopy(matrix)
-                place_wall(new_matrix, (i, j), orientation[0], orientation[1])
-                if dfs_1(new_matrix, player1_pos) and dfs_2(new_matrix, player2_pos):
-                    place_wall(matrix, (i, j), orientation[0], orientation[1])
-                    counter += 1
-                    if current_player == 1:
-                        player1_walls -= 1
+                if move in valid_moves:
+                    player2_pos = move_player(matrix, player2_pos, move, 2)
+                elif move == 'exit':
+                    break
+                else:
+                    print("Invalid move! Try again.")
+                    continue
+        elif action == 'place wall':
+            if (current_player == 1 and player1_walls > 0) or (current_player == 2 and player2_walls > 0):
+                i = int(input("Enter wall position (row): "))
+                if i == 'exit':
+                    break
+                else:
+                    j = int(input("Enter wall position (column): "))
+                    if j == 'exit':
+                        break
+                    orientation = input("Enter wall orientation (e.g. 'up-right', 'down-left'): ").strip().split('-')
+                    if is_valid_wall(matrix, (i, j), orientation[0], orientation[1]):
+                        new_matrix = copy.deepcopy(matrix)
+                        place_wall(new_matrix, (i, j), orientation[0], orientation[1])
+                        if dfs_1(new_matrix, player1_pos) and dfs_2(new_matrix, player2_pos):
+                            place_wall(matrix, (i, j), orientation[0], orientation[1])
+                            if current_player == 1:
+                                player1_walls -= 1
+                            else:
+                                player2_walls -= 1
                     else:
-                        player2_walls -= 1
+                        print("Invalid wall placement. Try again.")
+                        continue
             else:
-                print("Invalid wall placement. Try again.")
+                print("No walls left for this player!")
                 continue
+        elif action == 'exit':
+            break
         else:
-            print("No walls left for this player!")
+            print("Invalid action! Please choose 'move' or 'place wall'.")
             continue
-    else:
-        print("Invalid action! Please choose 'move' or 'place wall'.")
-        continue
+        show_board(matrix)
+        current_player = 2 if current_player == 1 else 1
+        game_info = {
+            "ID": game_id,
+            "player1": user1,
+            "player2": user2,
+            "user1_position": player1_pos,
+            "user2_position": player2_pos,
+            "user1_walls": player1_walls,
+            "user2_walls": player2_walls,
+            "game_board": matrix,
+            "current_player": current_player,
+            "winner": 0
+        }
+        if os.path.exists('./games.json'):
+            with open('games.json', 'r') as file:
+                games = json.load(file)
+            ids = []
+            for i in games:
+                ids.append(i['ID'])
+            if game_id in ids:
+                update_item('games.json', game_id, game_info)
+            else:
+                add_game(game_info)
 
-    show_board(matrix)
-    
+while True:
+    inp = input('new game or load game? ')
+    if inp == 'new game':
+        new_id = generate_id()
+        matrix = matrix()
+        matrix[0][8] = 1
+        matrix[16][8] = 2
+        player1_pos = (0, 8)
+        player2_pos = (16, 8)
+        player1_walls = 10
+        player2_walls = 10
+        current_player = random.choice([1, 2])
+        user1 = 'user1'
+        user2 = 'user2'
+        main_game(matrix , player1_pos , player2_pos , player1_walls , player2_walls , current_player , new_id , user1 , user2)
+    elif inp == 'load game':
+        id = int(input('Enter the game id: '))
+        with open('games.json', 'r') as file:
+            games = json.load(file)
+            ids = []
+            for game in games:
+                ids.append(game['ID'])
+            if id not in ids:
+                print('this id does not exist')
+                continue
+            for game in games:
+                if game['ID'] == id:
+                    id = game['ID']
+                    matrix = game['game_board']
+                    player1_pos = game['user1_position']
+                    player2_pos = game['user2_position']
+                    player1_walls = game['user1_walls']
+                    player2_walls = game['user2_walls']
+                    current_player = game['current_player']
+                    user1 = game['player1']
+                    user2 = game['player2']
+                    main_game(matrix , player1_pos , player2_pos , player1_walls , player2_walls , current_player,id,user1,user2)
 
-    current_player = 2 if current_player == 1 else 1
+# user_iterface.main()
+# print(user_iterface.user1)
