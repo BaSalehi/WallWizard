c52eba0d6be2170500077e364fd429f729a43538
Amirparsatec <amirparsa138585@yahoo.com>
2025-01-12 20:36:43 +0330

    leaderboard plan1

---
+++ leaderboard.py
@@ -0,0 +1,12 @@
+import json
+from rich.console import Console
+from rich.table import Table
+from rich.panel import Panel
+
+def load_users(json_file):
+    with open(json_file, 'r') as file:
+        return json.load(file)
+
+def create_leaderboard(users):
+    sorted_users = sorted(users, key=lambda x: x.get('score', 0), reverse=True)
+    return sorted_users[:3]
